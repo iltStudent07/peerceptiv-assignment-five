b306a7747aebefb0d33c52429bb05e68ad49662f
iltStudent07 <iltStudent07@labs.webagesolutions.com>
2026-06-26 23:25:00 +0000

    Init Homepage component and gameSystems.JSON file

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to the Game Depot!</h1>
+            <p>We have all your gaming needs!</p>
+        </div>
+    )
+}
+export default Home
