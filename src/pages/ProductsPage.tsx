9500ac4e5fb0ea7406f80fae9bd1419d88d5459d
iltStudent07 <iltStudent07@labs.webagesolutions.com>
2026-06-27 21:37:47 +0000

    Init ProductsPage and NotFound file

---
+++ src/pages/ProductsPage.tsx
@@ -0,0 +1,10 @@
+function ProductsPage() {
+    return (
+        <div>
+            <h1>Products</h1>
+            <h2>Below you'll find all the amazing products we have to offer:</h2>
+        </div>
+    )
+}
+
+export default ProductsPage
