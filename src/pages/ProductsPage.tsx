7488a36b1c19f564b66621b0404ac137de88f807
iltStudent07 <iltStudent07@labs.webagesolutions.com>
2026-06-27 22:55:26 +0000

    Update ProductsPage and moved gameSystem.json to the public folder

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,8 +1,40 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+interface Product {
+    id: string
+    name: string
+    price: number
+    category: string
+}
+
 function ProductsPage() {
+    const [products, setProducts] = useState<Product[]>([])
+    const [loading, setLoading] =useState<boolean>(true)
+
+    useEffect(() => {
+        fetch('/gameSystems.json')
+            .then((res) => res.json())
+            .then((data: Product[]) => {
+                setProducts(data);
+                setLoading(false);
+            })
+    }, [])
+
+    if (loading) return <p>Loading products...</p>
+
     return (
         <div>
             <h1>Products</h1>
-            <h2>Below you'll find all the amazing products we have to offer:</h2>
+            <p style={{ borderBottom: '2px solid #333'}}>Below you'll find all the amazing products we have to offer:</p>
+            <div>
+                <h2 style={{padding: '24px'}}>Game Systems:</h2>
+                <ul style={{listStyleType: 'none', textAlign: 'left'}}>
+                    {products.map(product => (
+                        <li key={product.id} style={{padding: '14px'}}><Link to={`/products/${product.id}`}>{product.name}</Link></li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
