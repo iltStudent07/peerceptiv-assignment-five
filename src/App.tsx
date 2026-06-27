8b8b562037b4b59a12fe4fae6fb73d7ce9aa1891
iltStudent07 <iltStudent07@labs.webagesolutions.com>
2026-06-27 23:32:42 +0000

    Added Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import HomePage from './pages/HomePage'
 import ProductsPage from './pages/ProductsPage'
 import NotFound from './pages/NotFound'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
+        <Footer />
       </div>
     </>
   )
