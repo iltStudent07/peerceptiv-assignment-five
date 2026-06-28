59de2c07bdc4714ca45e12aa78d92dd9b21fde04
iltStudent07 <iltStudent07@labs.webagesolutions.com>
2026-06-28 00:23:36 +0000

    Init ProductDetails page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './Navbar'
 import HomePage from './pages/HomePage'
 import ProductsPage from './pages/ProductsPage'
+import ProductDetail from './pages/ProductDetail'
 import NotFound from './pages/NotFound'
 import Footer from './components/Footer'
 import './App.css'
@@ -17,6 +18,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/products" element={<ProductsPage />} />
+            <Route path="/products/:id" element={<ProductDetail />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
---
+++ src/pages/ProductDetail.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+
+interface Product {
+    id: string
+    name: string
+    price: number
+    category: string
+}
+
+function ProductDetail() {
+    const { id } = useParams<{ id: string }>()
+    const navigate = useNavigate()
+    const [product, setProduct] = useState<Product | null>(null)
+    const [loading, setLoading] = useState<boolean>(true)
+
+    useEffect(() => {
+        fetch('/gameSystems.json')
+            .then ( res => {
+                if (!res.ok) throw new Error('Product not found')
+                    return res.json()
+            })
+            .then((data: Product[]) => {
+                const itemPage = data.find((item) => item.id === id) ?? null
+                setProduct(itemPage)
+                setLoading(false)
+            })
+            .catch(() => {
+                setLoading(false)
+            })
+    }, [id])
+
+    if (loading) return <p>Loading...</p>
+    if (!product) return <p>Product not found. <button onClick={() => navigate('/products')}>Back to Products</button></p>
+
+    return (
+        <div>
+            <button onClick={() => navigate(-1)} style={{ marginBottom: '16px'}}>Back</button>
+            <h1>{product.name}</h1>
+            <p><strong>Price:</strong> {product.price}</p>
+            <p><strong>Category:</strong> {product.category}</p>
+        </div>
+    )
+}
+
+export default ProductDetail
